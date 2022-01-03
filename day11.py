5c710634d12e0498d3bd754d683e0e3349fd6667
lvcts <64696964+lvcts@users.noreply.github.com>
2022-01-03 20:00:23 +0800

    Delete day11.py

--- day11.py
+++
@@ -1,7 +0,0 @@
-cake = {'Cake':'Muffin','Rasa':'Matcha','Topping':'Whipped Cream','Baju':'Orange'}
-print(cake)
-print("Cake : ",cake['Cake'])
-print("Rasa : ",cake['Rasa'])
-print("Topping : ",cake['Topping'])
-del cake['Baju']
-print(cake)
