1c857178dbe4bbb02eed00fb7fbf67d7b371aa68
lvcts <lovecatssss17@gmail.com>
2022-01-07 21:35:40 +0800

    27

---
+++ day27.py
@@ -0,0 +1,18 @@
+hari = {'1':'senin','2':'selasa','3':'rabu','4':'kamis','5':'jumat','6':'sabtu','7':'minggu'}
+masukkan = input('Masukkan angka hari (1-7): ')
+if masukkan == '1':
+    print(hari['1'])
+elif masukkan == '2':
+    print(hari['2'])
+elif masukkan == '3':
+    print(hari['3'])
+elif masukkan == '4':
+    print(hari['4'])
+elif masukkan == '5':
+    print(hari['5'])
+elif masukkan == '6':
+    print(hari['6'])
+elif masukkan == '7':
+    print(hari['7'])
+else:
+    exit
