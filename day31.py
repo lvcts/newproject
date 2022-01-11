37303e3f715ad1cf5a893471dc91f585983c8bb8
lvcts <lovecatssss17@gmail.com>
2022-01-11 20:08:48 +0800

    looping for

---
+++ day31.py
@@ -0,0 +1,19 @@
+#forstatement
+print("For pada list")
+l = ["aku", "mau", "makan"]
+for i in l:
+	print(i)
+print("\nfor pada tuple")
+t = ("aku", "mau", "makam")
+for i in t:
+	print(i)
+print("\nfor pada string")
+s = "aku mau makan"
+for i in s:
+	print(i)
+print("\nfor pada dictiooary")
+d = dict()
+d['a'] = 'aku mau makan'
+d['b'] = 'aku mau minum'
+for i in d:
+	print(i, d[i])
