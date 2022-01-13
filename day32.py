b092ad4e2f49157c0ced8d22c0cab3ab800ddbe8
lvcts <lovecatssss17@gmail.com>
2022-01-13 23:24:55 +0800

    Tuple

--- day32.py
+++ day32.py
@@ -1,10 +1,20 @@
-#operator perbandingan
-q,w,e,r,t,y = 1,3,4,4,6,9
-hasil1 = q <= w is not e is r <= 6 <= y
-hasil2 = q > w is e is r > t < y
-hasil3 = y > q < w < t is not e is r
-hasil4 = e = r < y > q < t > w
-print(hasil1)
-print(hasil2)
-print(hasil3)
-print(hasil4)
+Tuple1 = (17, 'Halo', 2, 'Guys')
+print("\nContoh penulisan tuple dengan banyak dat ")
+print(Tuple1)
+
+Tuple1 = (0, 1, 2, 3)
+Tuple2 = ('Hi', 'selamat mLm')
+Tuple3 = (Tuple1, Tuple2)
+print("\nCOntoh tuple bersarang : ")
+print(Tuple3)
+
+Tuple1 = ('Assalamualaikum',) * 5
+print("\nTuple dengan pengulangan ")
+print(Tuple1)
+
+Tuple1 = ('Zahra')
+n = 5
+print("\nTuplw dengan mwnggunakan perulangan")
+for i in range(int(n)):
+	Tuple1 = (Tuple1)
+	print(Tuple1)
