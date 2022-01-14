2f846c9c238f2cb8abc716ca929462b141ee0cf6
lvcts <lovecatssss17@gmail.com>
2022-01-14 21:30:31 +0800

    neww

---
+++ day32.py
@@ -0,0 +1,10 @@
+#operator perbandingan
+q,w,e,r,t,y = 1,3,4,4,6,9
+hasil1 = q <= w is not e is r <= 6 <= y
+hasil2 = q > w is e is r > t < y
+hasil3 = y > q < w < t is not e is r
+hasil4 = e = r < y > q < t > w
+print(hasil1)
+print(hasil2)
+print(hasil3)
+print(hasil4)
