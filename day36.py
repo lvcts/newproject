6c198369f746dfa49d4a899a6da3d69b201d8277
lvcts <lovecatssss17@gmail.com>
2022-01-16 23:25:00 +0800

    tugas

---
+++ day36.py
@@ -0,0 +1,69 @@
+#python operator
+#operator aritmetika
+a = 10
+b = 5
+tambah = a + b
+kurang = a - b
+kali = a * b
+bagi = a / b
+bagi2 = a // b
+modulo = a % b
+p = a ** b
+
+print(tambah)
+print(kurang)
+print(kali)
+print(bagi)
+print(bagi2)
+print(modulo)
+print(p)
+print('==========================================')
+#opertor pembanding
+a = 17
+b = 2
+print(a > b)
+print(a < b)
+print(a == b)
+print(a != b)
+print(a >= b)
+print(a <= b)
+print('==========================================')
+
+#operator logika
+a = True
+b = False
+
+print(a and b)
+print(a or b)
+print(not a)
+print('==========================================')
+
+#operator bitwise
+# Examples of Bitwise operators
+a = 10
+b = 5
+
+print(a & b)
+print(a | b)
+print(~a)
+print(a ^ b)
+print(a >> 2)
+print(a << 2)
+print('==========================================')
+
+#operator penugasan
+# Examples of Assignment Operators
+a = 10
+b = a
+print(b)
+b += a
+print(b)
+b -= a
+print(b)
+b *= a
+print(b)
+b <<= a
+print(b)
+
+
+
