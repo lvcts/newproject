2a40335b22c06b3e890730294363bcc62b74b65f
lvcts <lovecatssss17@gmail.com>
2022-01-19 23:00:03 +0800

    string

---
+++ day39.py
@@ -0,0 +1,34 @@
+#string
+
+String1 = "jodoh yang ditakdirkan"
+print("isi string : ")
+print(String1)
+
+print("\nKarakter pertama dari string :")
+print(String1[0])
+
+print("\nKarakter terakhir dari string :")
+print(String1[-1])
+
+
+print("n\Membagi karakter string dari 5:10 ")
+print(String1[5:10])
+ 
+
+print("\nMembagi karakter antara " +
+    "2 karakter awal dan 3 karakterakhir: ")
+print(String1[-2:3])
+
+
+'''String1[4] = 'h'
+print("\Mengubah karakter indeks ke 4 ")
+print(String1)'''
+
+String1 = "jodohh yang tidak ditakdirkan"
+print("\nupdate string: ")
+print(String1)
+
+
+'''del String1[5]
+print("\nDeleting character at 2nd Index: ")
+print(String1)'''
