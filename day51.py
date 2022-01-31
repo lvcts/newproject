5f76822ea158e7120825b1f07328326e22043bd0
lvcts <lovecatssss17@gmail.com>
2022-01-31 23:16:13 +0800

    cob asrray

---
+++ day51.py
@@ -0,0 +1,10 @@
+#latihan array lage
+buku_A = ['A','Novel','Airin','Pernah ada','30000']
+buku_B = ['B','Cerita','Ayunindi','Meluakan segalanya','40000']
+print('\t\t\tDaftar Harga Buku')
+print('====================================================')
+jumlah = 2
+for x in range(jumlah):
+    print('No \t Kode Buku \t Jenis buku \t Penulis \t Judul buku \t Harga')
+    print(x,'\t','\t',buku_A[x],'\t','\t',buku_A[x+1],'\t','\t',buku_A[x+2],'\t','\t',buku_A[x+3])
+
