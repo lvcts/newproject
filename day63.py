4ecb423746763f194fae852df07567db303131ae
lvcts <lovecatssss17@gmail.com>
2022-02-12 22:09:41 +0800

    flower

--- day63.py
+++ day63.py
@@ -1,9 +1,9 @@
 class taman_bunga():
     jumlah_bunga = 0
     
-    def __init__(self, nama, harga):
+    def __init__(self, nama, jenis):
         self.nama = nama
-        self.harga = harga
+        self.jenis = jenis
         taman_bunga.jumlah_bunga += 1
         
     def berapa_jumlah():
@@ -11,17 +11,13 @@ class taman_bunga():
         
     def detail_produk(self):
         print("Nama : ", self.nama)
-        print("Harga: ", self.harga)
+        print("Jenis: ", self.jenis)
         print()
         
-# membuat objek pertama
-produk1 = taman_bunga('mawar', 500)
+produk1 = taman_bunga('mawar', 'merah')
 
-# membuat objek kedua
-produk2 = taman_bunga('melati', 300)
+produk2 = taman_bunga('melati', 'putih')
 
-# mengakses attribut objek
 produk1.detail_produk()
 produk2.detail_produk()
 
-taman_bunga.berapa_jumlah()
